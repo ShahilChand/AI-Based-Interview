2ad621bb5dd8aea7a2da607852b5d7caccb1204e
Foxiii01 <shahilchand0001@gmail.com>
2025-09-19 18:55:39 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+// In your App.js
+import LoginPage from './components/LoginPage/loginPage.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
